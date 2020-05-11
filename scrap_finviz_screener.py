250db299ae37daad9f79f8730f7850f48e00de1a
hevangel <git@hevangel.com>
2020-05-11 15:18:59 -0700

    regenerate reports

--- scrap_finviz_screener.py
+++ scrap_finviz_screener.py
@@ -56,7 +56,7 @@ def main():
     parser.add_argument('-report', type=str, default='daily_report.xml', help='file name of the test report')
     args = parser.parse_args()
     args.no_scrap = True
-    args.date = '2020-05-04'
+    args.date = '2020-05-05'
 
     # check is the market closed today
     with open('market_close_dates.txt', 'r') as reader:
