c20c6974ceeaf327a0ceca4b87e58891019e76a6
horace chan <hevangel@users.noreply.github.com>
2020-05-13 01:02:23 -0700

    first check in

---
+++ scrap_yahoo_earning_calendar.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import argparse
+import datetime
+import sys
+import yfinance as yf
+
+def onDay(date, day):
+    """
+    :param date: current date
+    :param day: next monday(0) - sunday(7)
+    :return:
+    """
+    return date + datetime.timedelta(days=(day-date.weekday()+7)%7)
+
+def main():
+    parser = argparse.ArgumentParser(description='scrap yahoo earning')
+    parser.add_argument('-input', type=str, default='earnings.csv', help='input file')
+    parser.add_argument('-output_prefix', type=str, default='data_yahoo_earnings_estimate/earnings_estimate_', help='output file')
+    parser.add_argument('-today', type=str, default=str(datetime.date.today()), help='Specify today date')
+    args = parser.parse_args()
+
+    args = pd.DataFrame()
+    args.today = '2020-05-11'
+    args.input = 'earnings2.csv'
+    args.output_prefix = 'earnings_estimate_'
+
+    today = datetime.datetime.fromisoformat(args.today)
+    df_input = pd.read_csv(args.input)
+    df_input.set_index('Ticker', inplace=True)
+    df_output = pd.DataFrame()
+
+    for ticker in df_input.index:
+        yf_ticker = yf.Ticker(ticker)
+        try:
+            yf_calendar = yf_ticker.calendar
+        except:
+            print(f"{ticker} has no earnings date yet")
+        else:
+            if yf_calendar is None or yf_calendar.shape[1] == 0:
+                continue
+            if yf_calendar.shape[1] > 1:
+                print(f"{ticker} next earnings in week of {yf_calendar.at['Earnings Date',0].date()}")
+            else:
+                next_earnings_date = yf_calendar.at['Earnings Date','Value']
+                print(f"{ticker} next earnings at {next_earnings_date.date()}")
+                if next_earnings_date >= onDay(today,0) and next_earnings_date <= onDay(today,4):
+                    new_row = yf_calendar.T
+                    new_row.insert(1, 'Ticker', ticker)
+                    new_row.insert(2, 'Report', df_input.at[ticker,'Report'])
+                    df_output = df_output.append(new_row)
+
+    df_output.sort_values(by=['Earnings Date', 'Ticker'], inplace=True)
+    print(df_output)
+    df_output.to_csv(args.output_prefix + str(onDay(today,0).date()) + '.csv', index=False)
+
+if __name__ == "__main__":
+    sys.exit(main())
+
