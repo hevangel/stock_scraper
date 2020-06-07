6809e4ed9df8f5631351975b95e15d81fdee1aa8
horace chan <hevangel@users.noreply.github.com>
2020-06-06 23:27:22 -0700

    add scrap daily

---
+++ scrap_etfdb.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+import requests
+import bs4
+import pandas as pd
+import argparse
+import datetime
+import time
+import sys
+import pprint
+
+# -----------------------------------------------------------------
+# hand crafted scrapper
+# -----------------------------------------------------------------
+etfdb_url = 'https://etfdb.com/etf/'
+scrap_delay = 1
+
+def get_url(url):
+    response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    if not response:
+        print('Error', response.url, '-response code:', response.status_code)
+    time.sleep(scrap_delay)
+    return response.text
+
+def get_etf_info(ticker):
+    page_url = etfdb_url + ticker
+    page = get_url(page_url)
+    soup = bs4.BeautifulSoup(page, 'lxml')
+
+    row_dict = {}
+    row_dict['Ticker'] = ticker
+    row_dict['Description'] = soup.find_all('h1')[0].find_next('div').next_sibling.replace('\n','')
+    h3 = soup.find_all('h3')
+    for i in range(2):
+        for li in h3[i].find_next('ul').find_all('li'):
+            span_list = li.find_all('span')
+            row_dict[span_list[0].text] = span_list[1].text.replace('\n','')
+    return row_dict
+
+def main():
+    parser = argparse.ArgumentParser(description='scrap edfdb')
+    parser.add_argument('-output', type=str, default='data_tickers/etf.csv', help='output file')
+    args = parser.parse_args()
+
+    row_dict_list = []
+    df_all_tickers = pd.read_csv('data_tickers/all_tickers.csv')
+    for ticker in df_all_tickers.query('Industry == "Exchange Traded Fund"')['Ticker'].tolist():
+        print('get_etf_info',ticker)
+        row_dict_list.append(get_etf_info(ticker))
+
+    df = pd.DataFrame(row_dict_list)
+    df.to_csv(args.output)
+
+if __name__ == "__main__":
+    sys.exit(main())
+
