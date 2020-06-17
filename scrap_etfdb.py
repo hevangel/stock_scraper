3bd41630246e81eb2ee708675f9fb8fabe4802d9
horace chan <hevangel@users.noreply.github.com>
2020-06-17 01:27:39 -0700

    add scrap daily

--- scrap_etfdb.py
+++ scrap_etfdb.py
@@ -19,14 +19,16 @@ def get_url(url):
     response = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     if not response:
         print('Error', response.url, '-response code:', response.status_code)
+        return None
     time.sleep(scrap_delay)
     return response.text
 
 def get_etf_info(ticker):
     page_url = etfdb_url + ticker
     page = get_url(page_url)
+    if page is None:
+        return None
     soup = bs4.BeautifulSoup(page, 'lxml')
-
     row_dict = {}
     row_dict['Ticker'] = ticker
     row_dict['Description'] = soup.find_all('h1')[0].find_next('div').next_sibling.replace('\n','')
@@ -44,9 +46,13 @@ def main():
 
     row_dict_list = []
     df_all_tickers = pd.read_csv('data_tickers/all_tickers.csv')
-    for ticker in df_all_tickers.query('Industry == "Exchange Traded Fund"')['Ticker'].tolist():
+    etf_list = df_all_tickers.query('Industry == "Exchange Traded Fund"')['Ticker'].tolist()
+    print('ETFs:', len(etf_list))
+    for ticker in etf_list:
         print('get_etf_info',ticker)
-        row_dict_list.append(get_etf_info(ticker))
+        row_dict = get_etf_info(ticker)
+        if row_dict is not None:
+            row_dict_list.append(row_dict)
 
     df = pd.DataFrame(row_dict_list)
     df.to_csv(args.output)
