f7f26190bdf98838919fcac48eaf3f2f2665436e
horace chan <hevangel@users.noreply.github.com>
2020-10-11 16:37:33 -0700

    add

---
+++ scrap_investpy_history_etf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import pandas as pd
+import argparse
+import sys
+import investpy
+import glob
+import os
+import datetime
+import time
+
+scrap_delay = 2
+
+def main():
+    parser = argparse.ArgumentParser(description='scrap yahoo earning')
+    parser.add_argument('-output_dir', type=str, default='../stock_data_local/raw_history_investing_etf/', help='output directory')
+    args = parser.parse_args()
+
+    country = 'united states'
+    etfs_dict = investpy.get_etfs_dict(country)
+
+    today = datetime.datetime.now().strftime('%d/%m/%Y')
+    for count,etf_row in enumerate(etfs_dict):
+        ticker = etf_row['symbol']
+        print('downloading...', ticker, '-', count, '/', len(etfs_dict))
+        df_etf = investpy.get_etf_historical_data(etf_row['name'],country,'1/1/1990',today)
+        df_etf.to_csv(args.output_dir + ticker + '.csv')
+        time.sleep(scrap_delay)
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main())
+
